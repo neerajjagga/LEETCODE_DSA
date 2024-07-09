ecb7a5360e1240e3445de6643d7f80073ee5d0df
Neeraj Jagga <neerajjagga26@gmail.com>
2024-07-09 13:14:34 +0530

    Time: 0 ms (100%), Space: 12 MB (49.44%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int n = row*col;
+
+        int s = 0;
+        int e = n-1;
+        
+        while(s <= e) {
+            int mid = s + (e-s)/2;
+
+            // find row index and col index
+            int rowIndex = mid/col;
+            int colIndex = mid%col;
+            int currNumber = matrix[rowIndex][colIndex];
+
+            if(currNumber == target) {
+                return true;
+            }
+            else if(currNumber > target) {
+                e = mid - 1;
+            }
+            else {
+                s = mid + 1;
+            }
+        }
+        return false;
+    }
+};
