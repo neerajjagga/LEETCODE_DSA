849aa11ec9566aeb7d95ed4c4ee61832eac93a87
Neeraj Jagga <neerajjagga26@gmail.com>
2024-07-26 13:24:50 +0530

    Time: 0 ms (100%), Space: 7.7 MB (65.25%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.cpp
+++ 0807-custom-sort-string/0807-custom-sort-string.cpp
@@ -2,6 +2,12 @@ class Solution {
 public:
     static string str;
     static bool customComparator(char char1, char char2) {
+        // This will return true, if position of char1 in str string is
+        // less then the position of char 2 in str string
+
+
+        // when true is returned, then char1 will be placed before char2 in the
+        // output string
         return (str.find(char1) < str.find(char2));
     }
     string customSortString(string order, string s) {
